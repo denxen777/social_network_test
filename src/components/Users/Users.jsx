81bbf652da2ec70c8eb19b3c3f600f42cbdba2a4
denxen777 <denxen777@gmail.com>
2023-02-25 19:35:12 +0700

    lesson_53 - Class Components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,52 +1,31 @@
 import {User} from "./User/User";
 import s from './Users.module.css';
+import axios from "axios";
+import {Component} from "react";
 
-export const Users = (props) => {
-  if (!props.users.length) {
-    props.setUsers([
-      {
-        id: 1,
-        img: './александр.jpg',
-        fullName: 'Александр М.',
-        status: 'I love football',
-        location: {city: 'Саратов', country: 'Россия'},
-        followed: true
-      },
-      {
-        id: 2,
-        img: './алексей.jpg',
-        fullName: 'Алексей И.',
-        status: 'I love basketball',
-        location: {city: 'Иркутск', country: 'Россия'},
-        followed: false
-      },
-      {
-        id: 3,
-        img: './денис.jpg',
-        fullName: 'Денис К.',
-        status: 'I love swimming',
-        location: {city: 'Красноярск', country: 'Россия'},
-        followed: false
-      },
-      {
-        id: 4,
-        img: './иван.jpg',
-        fullName: 'Иван Е.',
-        status: 'I love run',
-        location: {city: 'Москва', country: 'Россия'},
-        followed: true
-      },
-    ]);
+export class Users extends Component {
+  constructor(props) {
+    super(props);
+
+    if (!this.props.users.length) {
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+    }
   }
 
-  const users = props.users.map(u =>
-    <User key={u.id} id={u.id} img={u.img} fullName={u.fullName} status={u.status}
-          location={u.location} followed={u.followed}
-          follow={props.follow} unfollow={props.unfollow} setUsers={props.setUsers}/>);
+  getUser = () => {
+    return this.props.users.map(u =>
+      <User key={u.id} id={u.id} photos={u.photos} name={u.name} status={u.status}
+            location={u.location} followed={u.followed}
+            follow={this.props.follow} unfollow={this.props.unfollow} setUsers={this.props.setUsers}/>)
+  }
 
-  return (
-    <div className={s.wrapper}>
-      {users}
-    </div>
-  );
+  render() {
+    return (
+      <div className={s.wrapper}>
+        {this.getUser()}
+      </div>
+    );
+  }
 }
